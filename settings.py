97eb6c1cbd85beb28697a6fc31096f972b515993
Anoop Thomas Mathew <atmb4u@gmail.com>
2011-06-03 17:29:50 +0530

    Added all changes to settings and urls

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'appster',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -116,9 +116,19 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'browse',
+    'socialauth',
+    'compressor',
+    'google_analytics',
+    'sorl.thumbnail',
+    'openid_consumer',    
+    'dinette',
+    'blogango',
+    'taggit',
+    'logging',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -143,3 +153,9 @@ LOGGING = {
         },
     }
 }
+COMPRESS_ROOT = STATIC_ROOT
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder')
+from markupfield.markup import DEFAULT_MARKUP_TYPES
+DEFAULT_MARKUP_TYPE = "plain"
+MARKUP_RENDERERS = DEFAULT_MARKUP_TYPES
+from extra_settings import *
