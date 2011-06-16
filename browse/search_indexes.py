db3469dbd81ec05e1f09d3224af4a218b838575e
Anoop Thomas Mathew <anoop@agiliq.com>
2011-06-16 15:55:03 +0000

    Added search indexes in browse

---
+++ browse/search_indexes.py
@@ -0,0 +1,21 @@
+from browse.models import *
+from haystack import site
+from haystack.indexes import *
+class AppIndex(SearchIndex):
+   text = CharField(document = True, use_template = True)
+   title = CharField(model_attr = "title")
+#   description = CharField(model_attr = "description")
+   developer = MultiValueField()
+#   developer = CharField(model_attr = "developer")
+#   device = MultiValueField()
+
+   def prepare_developer(self, obj): 
+       return [a for a in Developer.objects.all()]
+   def index_queryset(self):
+       return App.objects.all()
+#class DeveloperIndex(SearchIndex):
+#   text = CharField(document = True, use_template = True)
+
+
+site.register(App,AppIndex)   
+
