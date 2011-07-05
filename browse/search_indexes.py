a3832322532635548469c8c5617e4ef8143b727b
Anoop Thomas Mathew <anoop@agiliq.com>
2011-07-05 14:08:42 +0000

    few issues with search

--- browse/search_indexes.py
+++ browse/search_indexes.py
@@ -9,13 +9,13 @@ class AppIndex(SearchIndex):
 #   developer = CharField(model_attr = "developer")
 #   device = MultiValueField()
 
-   def prepare_developer(self, obj): 
-       return [a for a in Developer.objects.all()]
+#   def prepare_developer(self, obj): 
+#       return [a for a in Developer.objects.all()]
    def index_queryset(self):
        return App.objects.all()
-#class DeveloperIndex(SearchIndex):
-#   text = CharField(document = True, use_template = True)
-#   name = CharField(model_attr = "name")
+class DeveloperIndex(SearchIndex):
+   text = CharField(document = True, use_template = True)
+   name = CharField(model_attr = "name")
 
 site.register(App, AppIndex)   
-#site.register(Developer, DeveloperIndex)
+site.register(Developer, DeveloperIndex)
