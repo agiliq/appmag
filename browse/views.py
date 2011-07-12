e4c918bfbca713bfe095c4ab537ace303c307b1d
Anoop Thomas Mathew <anoop@agiliq.com>
2011-07-12 14:05:03 +0000

    ref:#24 added also available to appdetails

--- browse/views.py
+++ browse/views.py
@@ -82,6 +82,7 @@ def get_list_platform(request,slug):
     t = loader.get_template('browse/listplatform.html')
     c = RequestContext(request,{'profile':profile,'slug':slug,'platform':platform})
     return HttpResponse(t.render(c))
+   #used for old url scheme
 def get_app(request,id):
     profile = App.objects.filter(slug=id)[:1]
     developerlist = App.objects.values_list('title','logo','slug').filter(slug__isnull=False).exclude(slug__exact='').exclude(title=profile[0].title).filter(developer = profile[0].developer).order_by('?').distinct()[:10]
@@ -90,6 +91,7 @@ def get_app(request,id):
     t = loader.get_template('browse/appdetails.html')
     c = RequestContext(request,{'profile':profile, 'developerlist':developerlist,'related':related,'morelinks':more})
     return HttpResponse(t.render(c))
+    # new url scheme
 def get_app1(request,id,platform):
     plat="AP"
     if platform=="Android":
@@ -103,7 +105,8 @@ def get_app1(request,id,platform):
     profile = App.objects.filter(slug=id).filter(platform=plat)[:1]
     developerlist = App.objects.values_list('title','logo','slug').filter(slug__isnull=False).exclude(slug__exact='').exclude(title=profile[0].title).filter(developer = profile[0].developer).order_by('?').distinct()[:10]
     related = App.objects.values_list('title','logo','slug').filter(slug__isnull=False).exclude(slug__exact='').exclude(title=profile[0].title).filter(category = profile[0].category).filter(platform=profile[0].platform).order_by('?').distinct()[:10]
-    more = morelinks.objects.filter(app__slug=id)
+    also = App.objects.filter(title=profile[0].title).exclude(id=profile[0].id).exclude(platform=profile[0].platform)
+    more = morelinks.objects.filter(app__id=profile[0].id)
     t = loader.get_template('browse/appdetails.html')
-    c = RequestContext(request,{'profile':profile, 'developerlist':developerlist,'related':related,'morelinks':more})
+    c = RequestContext(request,{'profile':profile, 'developerlist':developerlist,'related':related,'morelinks':more,'also':also})
     return HttpResponse(t.render(c))
