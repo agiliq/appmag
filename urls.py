1f39e22df0d9ee0cc5bad9e42769b53caea64cb4
Rakesh Vidya Chandra <rakesh@agiliq.com>
2013-07-19 01:31:31 +0530

    pep8 fixes

--- urls.py
+++ urls.py
@@ -7,11 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    
-    url(r'^', include('browse.urls')),    
+
+    url(r'^', include('browse.urls')),
     url(r'^reviews/', include('blogango.urls')),
     url(r'^discuss/', include('dinette.urls')),
     url(r'^accounts/', include('socialauth.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
-    url(r'^search/', include('haystack.urls'),name='search'),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^search/', include('haystack.urls'), name='search'),
 )
