1331d0161b342b95ee3b0210b64fe672584e47fb
Rakesh Vidya Chandra <rakesh@agiliq.com>
2013-07-18 17:42:52 +0530

    extra settings

--- extra_settings.py
+++ extra_settings.py
@@ -11,16 +11,14 @@ RANKS_NAMES_DATA = ((30, "Member"), (100, "Senior Member"), (300, 'Star'))
 
 DINETTE_LOGIN_TEMPLATE = 'dinette/social_login.html'
 
-LOG_FILE_PATH = "\""+os.path.join(os.path.join(os.path.dirname(os.path.normpath(__file__)),'logs'),"logs.txt")+"\""
-LOG_FILE_PATH2 = "\""+os.path.join(os.path.join(os.path.dirname(os.path.normpath(__file__)),'logs2'),"logs2.txt")+"\""
+LOG_FILE_PATH = "\""+os.path.join(os.path.join(os.path.dirname(os.path.normpath(__file__)), 'logs'), "logs.txt")+"\""
+LOG_FILE_PATH2 = "\""+os.path.join(os.path.join(os.path.dirname(os.path.normpath(__file__)), 'logs2'), "logs2.txt")+"\""
 
-LOG_FILE_NAME =  os.path.join(os.path.dirname(os.path.normpath(__file__)),'logging.conf')
+LOG_FILE_NAME = os.path.join(os.path.dirname(os.path.normpath(__file__)), 'logging.conf')
 
 FLOOD_TIME = 1000
 
 #HAYSTACK_SITECONF = "dinette.search"
-
-HAYSTACK_SEARCH_ENGINE = 'xapian'
 
 #HAYSTACK_WHOOSH_PATH = os.path.join(os.path.dirname(os.path.normpath(__file__)),'index.db')
 
