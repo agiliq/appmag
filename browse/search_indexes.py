ec2cbf90a2392f28e0005b42fad8dd16e4c9078a
Rakesh Vidya Chandra <rakesh@agiliq.com>
2013-07-24 00:45:49 +0530

    Fixed indentaion errors in search_indexes.py

--- browse/search_indexes.py
+++ browse/search_indexes.py
@@ -10,14 +10,13 @@ class AppIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return App
 
+    def index_queryset(self, using=None):
+        return self.get_model.objects.all()
 
-   def index_queryset(self, using=None):
-       return self.get_model.objects.all()
 
+# class DeveloperIndex(indexes.SearchIndex, indexes.Indexable):
+#     text = indexes.CharField(document=True, use_template=True)
+#     name = indexes.CharField(model_attr="name")
 
-class DeveloperIndex(SearchIndex, indexes.Indexable):
-   text = indexes.CharField(document = True, use_template = True)
-   name = indexes.CharField(model_attr = "name")
-
-   def get_model(slef):
-   	return Developer
+    # def get_model(slef):
+    #   return Developer
