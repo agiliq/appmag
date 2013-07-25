d3b75127d9d512e077e16d360aaecc8f3f3ae019
Rakesh Vidya Chandra <rakesh@agiliq.com>
2013-07-26 01:27:55 +0530

    Fixed searched indexes.py

--- browse/search_indexes.py
+++ browse/search_indexes.py
@@ -11,12 +11,15 @@ class AppIndex(indexes.SearchIndex, indexes.Indexable):
         return App
 
     def index_queryset(self, using=None):
-        return self.get_model.objects.all()
+        return self.get_model().objects.all()
 
 
-# class DeveloperIndex(indexes.SearchIndex, indexes.Indexable):
-#     text = indexes.CharField(document=True, use_template=True)
-#     name = indexes.CharField(model_attr="name")
+class DeveloperIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr="name")
 
-    # def get_model(slef):
-    #   return Developer
+    def get_model(self):
+        return Developer
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
